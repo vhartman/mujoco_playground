a0aab5d37965ba19de69ee725947a742ff035813
Nikola Staykov <nstaykov@ethz.ch>
2026-04-08 15:45:26 +0200

    fix: removed hardcoded paths from tesollo xml files

--- mujoco_playground/_src/manipulation/tesollo_hand/base.py
+++ mujoco_playground/_src/manipulation/tesollo_hand/base.py
@@ -16,112 +16,118 @@
 
 from typing import Any, Dict, Optional, Union
 
-from etils import epath
 import jax
 import jax.numpy as jp
-from ml_collections import config_dict
 import mujoco
+from etils import epath
+from ml_collections import config_dict
 from mujoco import mjx
 
 from mujoco_playground._src import mjx_env
-from mujoco_playground._src.manipulation.tesollo_hand import tesollo_hand_constants as consts
+from mujoco_playground._src.manipulation.tesollo_hand import (
+    tesollo_hand_constants as consts,
+)
 
 
 def get_assets() -> Dict[str, bytes]:
-  assets = {}
-  path = mjx_env.MENAGERIE_PATH / "tesollo_hand"
-  mjx_env.update_assets(assets, path / "assets")
-  mjx_env.update_assets(assets, consts.ROOT_PATH / "xmls", "*.xml")
-  mjx_env.update_assets(
-      assets, consts.ROOT_PATH / "xmls" / "reorientation_cube_textures"
-  )
-  mjx_env.update_assets(assets, consts.ROOT_PATH / "xmls" / "meshes")
-  return assets
+    assets = {}
+    path = mjx_env.MENAGERIE_PATH / "tesollo_hand"
+    mjx_env.update_assets(assets, path / "assets")
+    mjx_env.update_assets(assets, consts.ROOT_PATH / "xmls", "*.xml")
+    mjx_env.update_assets(
+        assets, consts.ROOT_PATH / "xmls" / "reorientation_cube_textures"
+    )
+    mjx_env.update_assets(assets, consts.ROOT_PATH / "xmls" / "meshes", recursive=True)
+    return assets
 
 
 class TesolloHandEnv(mjx_env.MjxEnv):
-  """Base class for TESOLLO hand environments."""
+    """Base class for TESOLLO hand environments."""
 
-  def __init__(
-      self,
-      xml_path: str,
-      config: config_dict.ConfigDict,
-      config_overrides: Optional[Dict[str, Union[str, int, list[Any]]]] = None,
-  ) -> None:
-    super().__init__(config, config_overrides)
-    self._model_assets = get_assets()
-    self._mj_model = mujoco.MjModel.from_xml_string(
-        epath.Path(xml_path).read_text(), assets=self._model_assets
-    )
-    self._mj_model.opt.timestep = self._config.sim_dt
+    def __init__(
+        self,
+        xml_path: str,
+        config: config_dict.ConfigDict,
+        config_overrides: Optional[Dict[str, Union[str, int, list[Any]]]] = None,
+    ) -> None:
+        super().__init__(config, config_overrides)
+        self._model_assets = get_assets()
+        self._mj_model = mujoco.MjModel.from_xml_string(
+            epath.Path(xml_path).read_text(), assets=self._model_assets
+        )
+        self._mj_model.opt.timestep = self._config.sim_dt
 
-    self._mj_model.vis.global_.offwidth = 3840
-    self._mj_model.vis.global_.offheight = 2160
+        self._mj_model.vis.global_.offwidth = 3840
+        self._mj_model.vis.global_.offheight = 2160
 
-    self._mjx_model = mjx.put_model(self._mj_model, impl=self._config.impl)
-    self._xml_path = xml_path
+        self._mjx_model = mjx.put_model(self._mj_model, impl=self._config.impl)
+        self._xml_path = xml_path
 
-  # Sensor readings.
+    # Sensor readings.
 
-  def get_palm_position(self, data: mjx.Data) -> jax.Array:
-    return mjx_env.get_sensor_data(self.mj_model, data, "palm_position")
+    def get_palm_position(self, data: mjx.Data) -> jax.Array:
+        return mjx_env.get_sensor_data(self.mj_model, data, "palm_position")
 
-  def get_cube_position(self, data: mjx.Data) -> jax.Array:
-    return mjx_env.get_sensor_data(self.mj_model, data, "cube_position")
+    def get_cube_position(self, data: mjx.Data) -> jax.Array:
+        return mjx_env.get_sensor_data(self.mj_model, data, "cube_position")
 
-  def get_cube_orientation(self, data: mjx.Data) -> jax.Array:
-    return mjx_env.get_sensor_data(self.mj_model, data, "cube_orientation")
+    def get_cube_orientation(self, data: mjx.Data) -> jax.Array:
+        return mjx_env.get_sensor_data(self.mj_model, data, "cube_orientation")
 
-  def get_cube_linvel(self, data: mjx.Data) -> jax.Array:
-    return mjx_env.get_sensor_data(self.mj_model, data, "cube_linvel")
+    def get_cube_linvel(self, data: mjx.Data) -> jax.Array:
+        return mjx_env.get_sensor_data(self.mj_model, data, "cube_linvel")
 
-  def get_cube_angvel(self, data: mjx.Data) -> jax.Array:
-    return mjx_env.get_sensor_data(self.mj_model, data, "cube_angvel")
+    def get_cube_angvel(self, data: mjx.Data) -> jax.Array:
+        return mjx_env.get_sensor_data(self.mj_model, data, "cube_angvel")
 
-  def get_cube_angacc(self, data: mjx.Data) -> jax.Array:
-    return mjx_env.get_sensor_data(self.mj_model, data, "cube_angacc")
+    def get_cube_angacc(self, data: mjx.Data) -> jax.Array:
+        return mjx_env.get_sensor_data(self.mj_model, data, "cube_angacc")
 
-  def get_cube_upvector(self, data: mjx.Data) -> jax.Array:
-    return mjx_env.get_sensor_data(self.mj_model, data, "cube_upvector")
+    def get_cube_upvector(self, data: mjx.Data) -> jax.Array:
+        return mjx_env.get_sensor_data(self.mj_model, data, "cube_upvector")
 
-  def get_cube_goal_orientation(self, data: mjx.Data) -> jax.Array:
-    return mjx_env.get_sensor_data(self.mj_model, data, "cube_goal_orientation")
+    def get_cube_goal_orientation(self, data: mjx.Data) -> jax.Array:
+        return mjx_env.get_sensor_data(self.mj_model, data, "cube_goal_orientation")
 
-  def get_cube_goal_upvector(self, data: mjx.Data) -> jax.Array:
-    return mjx_env.get_sensor_data(self.mj_model, data, "cube_goal_upvector")
+    def get_cube_goal_upvector(self, data: mjx.Data) -> jax.Array:
+        return mjx_env.get_sensor_data(self.mj_model, data, "cube_goal_upvector")
 
-  def get_fingertip_positions(self, data: mjx.Data) -> jax.Array:
-    """Get fingertip positions relative to the grasp site."""
-    return jp.concatenate([
-        mjx_env.get_sensor_data(self.mj_model, data, f"{name}_position")
-        for name in consts.FINGERTIP_NAMES
-    ])
+    def get_fingertip_positions(self, data: mjx.Data) -> jax.Array:
+        """Get fingertip positions relative to the grasp site."""
+        return jp.concatenate(
+            [
+                mjx_env.get_sensor_data(self.mj_model, data, f"{name}_position")
+                for name in consts.FINGERTIP_NAMES
+            ]
+        )
 
-  # Accessors.
+    # Accessors.
 
-  @property
-  def xml_path(self) -> str:
-    return self._xml_path
+    @property
+    def xml_path(self) -> str:
+        return self._xml_path
 
-  @property
-  def action_size(self) -> int:
-    return self._mjx_model.nu
+    @property
+    def action_size(self) -> int:
+        return self._mjx_model.nu
 
-  @property
-  def mj_model(self) -> mujoco.MjModel:
-    return self._mj_model
+    @property
+    def mj_model(self) -> mujoco.MjModel:
+        return self._mj_model
 
-  @property
-  def mjx_model(self) -> mjx.Model:
-    return self._mjx_model
+    @property
+    def mjx_model(self) -> mjx.Model:
+        return self._mjx_model
 
 
 def uniform_quat(rng: jax.Array) -> jax.Array:
-  """Generate a random quaternion from a uniform distribution."""
-  u, v, w = jax.random.uniform(rng, (3,))
-  return jp.array([
-      jp.sqrt(1 - u) * jp.sin(2 * jp.pi * v),
-      jp.sqrt(1 - u) * jp.cos(2 * jp.pi * v),
-      jp.sqrt(u) * jp.sin(2 * jp.pi * w),
-      jp.sqrt(u) * jp.cos(2 * jp.pi * w),
-  ])
+    """Generate a random quaternion from a uniform distribution."""
+    u, v, w = jax.random.uniform(rng, (3,))
+    return jp.array(
+        [
+            jp.sqrt(1 - u) * jp.sin(2 * jp.pi * v),
+            jp.sqrt(1 - u) * jp.cos(2 * jp.pi * v),
+            jp.sqrt(u) * jp.sin(2 * jp.pi * w),
+            jp.sqrt(u) * jp.cos(2 * jp.pi * w),
+        ]
+    )
